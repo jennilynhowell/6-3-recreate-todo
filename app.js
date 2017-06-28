a02d797408ca99754709adc91e35e0cc8be4d464
Jennilyn Howell <jennilynhowell@me.com>
2017-06-28 16:55:27 -0400

    refine styling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
+app.use('/public', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
